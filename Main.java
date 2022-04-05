c0da3d4bed7129a7eab8daa3853cf46c5e1e6361
owokaiser <101186844+owokaiser@users.noreply.github.com>
2022-04-05 11:11:15 +0000

    final commit

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}
+    int i;
+    int y;
+    int z;
+    
+    for(i=0;i<4;i++){
+      if(i==0 || i==2){
+        
+        for(z=1;z<=4;z++){
+          for(y=4;y>=z;y--){System.out.print(" ");}
+          for(y=0;y<z;y++){System.out.print("*");}
+          for(y=0;y<=z;y++){System.out.print(" ");}
+          System.out.print("\n");
+          
+                    
+        }}
+    else if(i==1 || i==3){
+              for(z=0;z<4;z++){
+                  for(y=0;y<=4;y++){System.out.print(" ");}
+          for(y=0;y<=z;y++){System.out.print("*");}
+                for(y=0;y<=4;y++){System.out.print(" ");}
+        System.out.print("\n");}
+    }
+  }}}
